621dcacd95f2dedab7d75152815372726d9e344c
shazrin <mshazrinbs@gmail.com>
2025-05-02 04:53:55 +0800

    test: add in-mem db for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.liquibase:liquibase-core")
     runtimeOnly("org.postgresql:postgresql")
+    testRuntimeOnly("com.h2database:h2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/test/java/io/github/shazxrin/onepercentbetter/ApplicationTest.java
+++ src/test/java/io/github/shazxrin/onepercentbetter/ApplicationTest.java
@@ -2,9 +2,11 @@ package io.github.shazxrin.onepercentbetter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class ApplicationTests {
+@ActiveProfiles("test")
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+class ApplicationTest {
 
     @Test
     void contextLoads() {
