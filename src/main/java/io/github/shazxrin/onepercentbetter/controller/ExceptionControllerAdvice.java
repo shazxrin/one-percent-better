18889eb3a85e98f038dd3b43d71c989ec427d5cc
shazrin <mshazrinbs@gmail.com>
2025-05-02 06:41:06 +0800

    refactor: return ProblemDetail in ExceptionControllerAdvice instead

--- src/main/java/io/github/shazxrin/onepercentbetter/controller/ExceptionControllerAdvice.java
+++ src/main/java/io/github/shazxrin/onepercentbetter/controller/ExceptionControllerAdvice.java
@@ -3,6 +3,7 @@ package io.github.shazxrin.onepercentbetter.controller;
 import io.github.shazxrin.onepercentbetter.exception.BadRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BadRequestException.class)
-    public void handleBadRequestException(BadRequestException ex) {
-        log.error("Bad request.", ex);
+    public ProblemDetail handleBadRequestException(BadRequestException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 }
