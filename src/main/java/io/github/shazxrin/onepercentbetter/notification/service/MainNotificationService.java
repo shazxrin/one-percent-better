f218a506539c230868c15340f4fa9a0d1832dd4f
shazrin <mshazrinbs@gmail.com>
2025-06-01 19:44:47 +0800

    fix: use exchange name from backbone library

--- src/main/java/io/github/shazxrin/onepercentbetter/notification/service/MainNotificationService.java
+++ src/main/java/io/github/shazxrin/onepercentbetter/notification/service/MainNotificationService.java
@@ -1,11 +1,13 @@
 package io.github.shazxrin.onepercentbetter.notification.service;
 
 import io.github.shazxrin.backbone.notification.NotificationMessage;
+import io.github.shazxrin.backbone.notification.NotificationMessageQueue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainNotificationService implements NotificationService {
+    private final static String ROUTING_KEY = "";
     private final RabbitTemplate rabbitTemplate;
 
     public MainNotificationService(RabbitTemplate rabbitTemplate) {
@@ -20,6 +22,6 @@ public class MainNotificationService implements NotificationService {
             message
         );
 
-        rabbitTemplate.convertAndSend("notification", notificationMessage);
+        rabbitTemplate.convertAndSend(NotificationMessageQueue.EXCHANGE_NAME, ROUTING_KEY, notificationMessage);
     }
 }
