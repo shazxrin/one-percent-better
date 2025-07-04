01d7901aa720895e6e6d41c8eb7ff4cdb8c1bc26
shazrin <mshazrinbs@gmail.com>
2025-07-04 09:59:27 +0800

    refactor: merge service interface for notification

--- app/src/main/java/io/github/shazxrin/onepercentbetter/notification/service/NotificationService.java
+++ app/src/main/java/io/github/shazxrin/onepercentbetter/notification/service/NotificationService.java
@@ -1,5 +1,28 @@
 package io.github.shazxrin.onepercentbetter.notification.service;
 
-public interface NotificationService {
-    void sendNotification(String title, String message);
+import io.github.shazxrin.notifier.common.NotificationMessage;
+import io.github.shazxrin.notifier.common.NotificationMessageQueue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+    private final static String APP_NAME = "one-percent-better";
+    private final static String ROUTING_KEY = "";
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public NotificationService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendNotification(String title, String message) {
+        NotificationMessage notificationMessage = new NotificationMessage(
+            APP_NAME,
+            title,
+            message
+        );
+
+        rabbitTemplate.convertAndSend(NotificationMessageQueue.EXCHANGE_NAME, ROUTING_KEY, notificationMessage);
+    }
 }
