fd71b5e54027f08132ba73d4434e11722557d921
shazrin <mshazrinbs@gmail.com>
2025-07-24 13:55:14 +0800

    refactor: customize name for database in micrometer

---
+++ app/src/main/java/io/github/shazxrin/onepercentbetter/observability/configuration/DataSourceMicrometerConfiguration.java
@@ -0,0 +1,15 @@
+package io.github.shazxrin.onepercentbetter.observability.configuration;
+
+import net.ttddyy.observation.boot.autoconfigure.ProxyDataSourceBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceMicrometerConfiguration {
+    @Bean
+    public ProxyDataSourceBuilderCustomizer myCustomizer(){
+        return (builder, dataSource, beanName, dataSourceName) -> {
+            builder.name("postgres");
+        };
+    }
+}
