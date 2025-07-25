4cca3220dccb402674e66d807f00dd12a71d81f8
shazrin <mshazrinbs@gmail.com>
2025-07-25 11:10:02 +0800

    feat: endpoints for triggering check in projects

--- app/src/main/java/io/github/shazxrin/onepercentbetter/checkin/controller/CheckInProjectController.java
+++ app/src/main/java/io/github/shazxrin/onepercentbetter/checkin/controller/CheckInProjectController.java
@@ -8,9 +8,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.time.LocalDate;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(
     name = "Check Ins",
@@ -25,12 +24,51 @@ public class CheckInProjectController {
         this.checkInProjectService = checkInProjectService;
     }
 
-    @Operation(summary = "Check in today")
+    @Operation(summary = "Check in for all projects on date")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Check in today successfully"),
+        @ApiResponse(responseCode = "200", description = "Check in for all projects on date successfully"),
     })
-    @PostMapping("/today")
-    public void postCheckInToday() {
-        checkInProjectService.checkInAll(LocalDate.now());
+    @PostMapping("/all")
+    public void postCheckInProjectAll(
+        @RequestParam(required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ) {
+        checkInProjectService.checkInAll(date);
+    }
+
+    @Operation(summary = "Check in for a projects on date")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Check in for project on date successfully"),
+    })
+    @PostMapping("/{projectId:^[0-9]*}")
+    public void postCheckInProject(
+        @PathVariable long projectId,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ) {
+        checkInProjectService.checkIn(projectId, date);
+    }
+
+    @Operation(summary = "Check in for all projects between date interval")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Check in for all projects between date interval successfully"),
+    })
+    @PostMapping("/interval/all")
+    public void postCheckInProjectAllInterval(
+        @RequestParam(required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromDate,
+        @RequestParam(required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate toDate
+    ) {
+        checkInProjectService.checkInAllInterval(fromDate, toDate);
+    }
+
+    @Operation(summary = "Check in for a projects between date interval")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Check in for project between date interval successfully"),
+    })
+    @PostMapping("/interval/{projectId:^[0-9]*}")
+    public void postCheckInProjectInterval(
+        @PathVariable long projectId,
+        @RequestParam(required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromDate,
+        @RequestParam(required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate toDate
+    ) {
+        checkInProjectService.checkInInterval(projectId, fromDate, toDate);
     }
 }
