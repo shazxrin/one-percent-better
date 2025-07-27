8045fa70ecd2a48aebf392e4b3fe8eb5e7c0d8e9
shazrin <mshazrinbs@gmail.com>
2025-07-28 00:22:08 +0800

    refactor: remove unused check in daily summary exception

--- app/src/main/java/io/github/shazxrin/onepercentbetter/checkin/exception/CheckInProjectDailySummaryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.shazxrin.onepercentbetter.checkin.exception;
-
-public class CheckInProjectDailySummaryNotFoundException extends RuntimeException {
-    public CheckInProjectDailySummaryNotFoundException(String message) {
-        super(message);
-    }
-}
