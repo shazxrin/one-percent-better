a91ee9fd9b61ec4e8976e9db0a9122bd27c142a3
shazrin <mshazrinbs@gmail.com>
2025-07-29 16:23:14 +0800

    refactor: rename check in project bootstrap test

--- app/src/test/java/io/github/shazxrin/onepercentbetter/checkin/trigger/CheckInProjectBootstrapTest.java
+++ app/src/test/java/io/github/shazxrin/onepercentbetter/checkin/trigger/CheckInProjectBootstrapTest.java
@@ -16,26 +16,26 @@ import java.util.Collections;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class CheckInBootstrapTest {
+public class CheckInProjectBootstrapTest {
 
     @Mock
-    private CheckInProjectProperties checkInProperties;
+    private CheckInProjectProperties checkInProjectProperties;
 
     @Mock
-    private CheckInProjectService checkInService;
+    private CheckInProjectService checkInProjectService;
 
     @Mock
     private ProjectService projectService;
 
     @InjectMocks
-    private CheckInProjectBootstrap checkInBootstrap;
+    private CheckInProjectBootstrap checkInProjectBootstrap;
 
     private static final LocalDate TODAY = LocalDate.now();
     private static final String BOOTSTRAP_DATE = "2023-01-01";
     private static final LocalDate PARSED_BOOTSTRAP_DATE = LocalDate.parse(BOOTSTRAP_DATE);
 
     @Test
-    void testCheckInBootstrap_withValidProjects_shouldAddProjectsAndCheckIn() {
+    void testRunBootstrapCheckInProjectsAll_withValidProjects_shouldAddProjectsAndCheckIn() {
         // Given
         String validProject1 = "owner1/project1";
         String validProject2 = "owner2/project2";
@@ -43,29 +43,29 @@ public class CheckInBootstrapTest {
         CheckInProjectProperties.Bootstrap bootstrap = new CheckInProjectProperties.Bootstrap();
         bootstrap.setDate(BOOTSTRAP_DATE);
         bootstrap.setProjects(Arrays.asList(validProject1, validProject2));
-        when(checkInProperties.getBootstrap()).thenReturn(bootstrap);
+        when(checkInProjectProperties.getBootstrap()).thenReturn(bootstrap);
 
         // When
-        checkInBootstrap.runBootstrapCheckInProjectsAll();
+        checkInProjectBootstrap.runBootstrapCheckInProjectsAll();
 
         // Then
         verify(projectService).addProject("owner1/project1");
         verify(projectService).addProject("owner2/project2");
-        verify(checkInService).checkInAllInterval(PARSED_BOOTSTRAP_DATE, TODAY);
+        verify(checkInProjectService).checkInAllInterval(PARSED_BOOTSTRAP_DATE, TODAY);
     }
 
     @Test
-    void testCheckInBootstrap_checksCorrectDateInterval() {
+    void testRunBootstrapCheckInProjectsAll_checksCorrectDateInterval() {
         // Given
         CheckInProjectProperties.Bootstrap bootstrap = new CheckInProjectProperties.Bootstrap();
         bootstrap.setDate(BOOTSTRAP_DATE);
         bootstrap.setProjects(Collections.emptyList());
-        when(checkInProperties.getBootstrap()).thenReturn(bootstrap);
+        when(checkInProjectProperties.getBootstrap()).thenReturn(bootstrap);
         
         // When
-        checkInBootstrap.runBootstrapCheckInProjectsAll();
+        checkInProjectBootstrap.runBootstrapCheckInProjectsAll();
 
         // Then
-        verify(checkInService).checkInAllInterval(PARSED_BOOTSTRAP_DATE, TODAY);
+        verify(checkInProjectService).checkInAllInterval(PARSED_BOOTSTRAP_DATE, TODAY);
     }
 }
