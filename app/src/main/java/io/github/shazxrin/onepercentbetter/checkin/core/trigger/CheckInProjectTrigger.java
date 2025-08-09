aafda5a4268c3acad012db617c7fd01a46a7a889
shazrin <mshazrinbs@gmail.com>
2025-08-09 20:45:48 +0800

    refactor: remove bootstrap feature

--- app/src/main/java/io/github/shazxrin/onepercentbetter/checkin/core/trigger/CheckInProjectTrigger.java
+++ app/src/main/java/io/github/shazxrin/onepercentbetter/checkin/core/trigger/CheckInProjectTrigger.java
@@ -1,14 +1,10 @@
 package io.github.shazxrin.onepercentbetter.checkin.core.trigger;
 
-import io.github.shazxrin.onepercentbetter.checkin.core.configuration.CheckInProjectProperties;
 import io.github.shazxrin.onepercentbetter.checkin.core.service.CheckInProjectService;
-import io.github.shazxrin.onepercentbetter.project.service.ProjectService;
 import io.micrometer.observation.annotation.Observed;
 import java.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -18,18 +14,10 @@ import org.springframework.stereotype.Component;
 public class CheckInProjectTrigger {
     private static final Logger log = LoggerFactory.getLogger(CheckInProjectTrigger.class);
 
-    private final CheckInProjectProperties checkInProperties;
     private final CheckInProjectService checkInProjectService;
-    private final ProjectService projectService;
 
-    public CheckInProjectTrigger(
-        CheckInProjectProperties checkInProperties,
-        CheckInProjectService checkInProjectService,
-        ProjectService projectService
-    ) {
-        this.checkInProperties = checkInProperties;
+    public CheckInProjectTrigger(CheckInProjectService checkInProjectService) {
         this.checkInProjectService = checkInProjectService;
-        this.projectService = projectService;
     }
 
     @Async
@@ -38,20 +26,5 @@ public class CheckInProjectTrigger {
         log.info("Running check-in schedule.");
 
         checkInProjectService.checkInAll(LocalDate.now());
-    }
-
-    @Async
-    @EventListener(
-        classes = ApplicationReadyEvent.class,
-        condition = "@checkInProjectProperties.bootstrap.enabled"
-    )
-    public void runBootstrapCheckInProjectsAll() {
-        log.info("Running check-in bootstrap.");
-        for (String project : checkInProperties.getBootstrap().getProjects()) {
-            projectService.addProject(project);
-        }
-
-        var bootstrapDate = LocalDate.parse(checkInProperties.getBootstrap().getDate());
-        checkInProjectService.checkInAllInterval(bootstrapDate, LocalDate.now());
     }
 }
