e4b0aca73a1d300387adf829c160f506340e74dd
shazrin <70547991+shazxrin@users.noreply.github.com>
2025-08-24 00:00:49 +0800

    fix(deps): update dependency org.springdoc:springdoc-openapi-starter-webmvc-api to v2.8.11 (#72)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.8.10")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.8.11")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.10")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
